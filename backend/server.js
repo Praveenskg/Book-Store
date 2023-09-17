941841709aa7c82a2048d665ed1dcac349700c45
Praveenskg <Praveensinghiics@gmail.com>
2023-09-17 18:01:51 +0530

    :recycle: Book Store

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { PORT, mongoURI } = require("./config");
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+// routes
+const booksRoute = require("./routes/books");
+app.use("/api/books", booksRoute);
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
